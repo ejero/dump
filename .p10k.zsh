7148e0474c8930a4e99dc56d3c508ac450c43fef
Rosita E <RositaEmakpo@Gmail.com>
2023-12-18 15:10:44 -0800

    Update .p10k.zsh

--- .p10k.zsh
+++ .p10k.zsh
@@ -188,7 +188,7 @@
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232
   typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=7
   # Custom icon.
-  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='🌮 🍩'
+  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='🍔 🍟 🧁'
 
   ################################[ prompt_char: prompt symbol ]################################
   # Transparent background.
